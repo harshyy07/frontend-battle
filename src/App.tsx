bddb489097bbd9b6c716d3bec4bd730f309b8958
harshyy07 <harshitha101105@gmail.com>
2026-06-26 17:31:26 +0530

    Remove custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { Features } from './components/Features';
 import { Pricing } from './components/Pricing';
 import { Footer } from './components/Footer';
 import { ScrollProgress } from './components/ScrollProgress';
-import { CustomCursor } from './components/CustomCursor';
 import { DataPipeline } from './components/DataPipeline';
 
 function App() {
   return (
-    <div className="font-sans text-arctic-powder bg-oceanic-noir min-h-screen selection:bg-forsythia selection:text-oceanic-noir cursor-none-global">
-      <CustomCursor />
+    <div className="font-sans text-arctic-powder bg-oceanic-noir min-h-screen selection:bg-forsythia selection:text-oceanic-noir">
       <ScrollProgress />
       <main>
         <Hero />
